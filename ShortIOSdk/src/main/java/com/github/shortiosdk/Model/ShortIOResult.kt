1e53a5251800a2a1d2b757b76d6d6159553dc341
aj <asadjaved9490@gmail.com>
2025-06-04 18:43:59 +0500

    * added stringOrInt for params

--- ShortIOSdk/src/main/java/com/github/shortiosdk/Model/ShortIOResult.kt
+++ ShortIOSdk/src/main/java/com/github/shortiosdk/Model/ShortIOResult.kt
@@ -6,3 +6,8 @@ public sealed class ShortIOResult {
     data class Success(val data: ShortIOResponseModel) : ShortIOResult()
     data class Error(val data: ShortIOErrorModel) : ShortIOResult()
 }
+
+public sealed class StringOrInt {
+    data class Str(val value: String) : StringOrInt()
+    data class IntVal(val value: Int) : StringOrInt()
+}
